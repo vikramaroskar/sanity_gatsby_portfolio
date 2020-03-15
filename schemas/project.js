f0a49d236a4f80b20afcc33a31166a5b0543c748
vikram <vikram.aroskar@gmail.com>
2020-03-15 12:48:36 +0530

    add preview etc

--- schemas/project.js
+++ schemas/project.js
@@ -37,8 +37,26 @@ export default {
       type: 'reference',
       to: {type:'author'}
     }
-  ]
+  ],
 
   //TODO play with previews
+  preview : {
+    //preset fields of title, subtitle, media 
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'image'
+    },
+    //prepare function as an additionala thing.
+    //only select tag above will work as well
+    prepare(selection) {
+      const {author} = selection
+      return {
+        ...selection,
+        subtitle: `author` && `${author} is a genius`
+      }
+    }
+
+  }
 
 };
